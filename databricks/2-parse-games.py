afab546c8aeabcc04325831df6df0d3e70b9c410
hieuimba <hieuimba>
2024-06-01 03:58:24 +0000

    added dbricks notebooks

---
+++ databricks/2-parse-games.py
@@ -0,0 +1,183 @@
+# Databricks notebook source
+file_month = dbutils.widgets.get("month")
+clear_silver = dbutils.widgets.get("clear_silver")
+
+bronze_dir = "/Volumes/main/default/vol-bronze/" + f"{file_month}/"
+silver_dir = "/Volumes/main/default/vol-silver/" + f"{file_month}/"
+
+if clear_silver == 'true':
+    dbutils.fs.rm(silver_dir, True)
+    dbutils.fs.mkdirs(silver_dir)
+
+print(f"Parsing games from: {bronze_dir} \n to {silver_dir}")
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col, when, regexp_extract, monotonically_increasing_id, first, sum, trim, regexp_replace, abs ,lit, floor, concat
+from pyspark.sql import Window
+from pyspark.sql.types import IntegerType
+
+import re
+
+def find_number_of_moves(moves_string):
+    if moves_string is None:
+        return 0
+    
+    # Search for the move number at the end of the string
+    reversed_moves_string = moves_string[::-1]
+    last_match = re.search(r"\ \.(\d+)|\ \.\.\.(\d+)", reversed_moves_string)
+
+    if last_match:
+        # Reverse the move number
+        return int(last_match.group()[::-1].split(".")[0])
+    else:
+        return 0
+
+
+def parse_games(file_name):
+    df = spark.read.text(bronze_dir + file_name).filter(
+        (col("value") != "")
+        & (~col("value").like("%UTCTime%"))
+    )
+
+    # Rename "value" column to "Line"
+    df = df.withColumnRenamed("value", "Line")
+
+    # Extract the "Key" and "Value" columns based on "Line" column
+    df = df.withColumn(
+        "Key",
+        when(col("Line").startswith("1."), "Moves").otherwise(
+            regexp_extract(col("Line"), r"\[(.*?)\s", 1)
+        ),
+    ).withColumn(
+        "Value",
+        when(col("Line").startswith("1."), col("Line")).otherwise(
+            regexp_extract(col("Line"), r'"(.*)"', 1)
+        ),
+    )
+
+    # Add a column to identify the start of a game
+    df = df.withColumn("StartOfGame", when(col("Key") == "Event", 1).otherwise(0))
+
+    # Define a window spec for calculating cumulative sum
+    windowSpec = Window.orderBy(monotonically_increasing_id())
+
+    # Calculate the cumulative sum of "StartOfGame" to create "GameID"
+    df = df.withColumn("GameID", sum("StartOfGame").over(windowSpec))
+
+    # Define the list of columns for pivoting
+    col_list = [
+        "UTCDate", "Event", "TimeControl", "Opening", "ECO",
+        "Site", "Termination", "Moves", "Result",
+        "White", "WhiteTitle", "WhiteElo", "WhiteRatingDiff",
+        "Black", "BlackTitle", "BlackElo", "BlackRatingDiff"
+    ]
+
+    # Pivot the DataFrame based on "GameID" and col_list
+    df = df.groupBy("GameID").pivot("Key", col_list).agg(first("Value"))
+    return df
+
+def filter_games(df):
+    # Filter out uncomplete games due to chunking
+    df = df.filter((col("GameID") > 0) & (col("GameID") < df.count() - 1))
+
+    # Filter for games with evaluations & other conditions
+    df = df.filter(
+        (col("Moves").contains("eval")) & 
+        (col("Event") != "Rated Correspondence game") & 
+        (col("Termination").isin(["Normal", "Time forfeit"])) & 
+        ((col("WhiteTitle").isNull()) | (col("WhiteTitle") != "BOT")) &
+        ((col("BlackTitle").isNull()) | (col("BlackTitle") != "BOT"))
+    )
+
+    # Find the number of moves using the find_number_of_moves UDF
+    find_number_of_moves_udf = udf(find_number_of_moves, IntegerType())
+    df = df.withColumn("NumberOfMoves", find_number_of_moves_udf("Moves"))
+
+    # Filter for games with >= 20 moves
+    df = df.filter(col("NumberOfMoves") >= 20)
+
+    # Cast Elo columns to int
+    columns_to_cast = ["WhiteElo", "BlackElo", "WhiteRatingDiff", "BlackRatingDiff"]
+    for column in columns_to_cast:
+        df = df.withColumn(column, col(column).cast("int"))
+
+    # Calculate AvgElo and round it down to the nearest 300 range
+    df = df.withColumn("EloRange", (floor(((col("WhiteElo") + col("BlackElo")) / 2) / 300) * 300).cast("int"))
+
+    # Adjust "EloRange" for special cases
+    df = df.withColumn("EloRange", when(col("EloRange") <= 300, 0)
+                                    .when(col("EloRange") > 3000, 3000)
+                                    .otherwise(col("EloRange")))
+
+    # Calculate the difference between White and Black's ELO
+    df = df.withColumn("EloDiff", abs(col("WhiteElo") - col("BlackElo")))
+
+    # Calculate the swing in ELO after the match ends
+    if "WhiteRatingDiff" in df.columns and "BlackRatingDiff" in df.columns:
+        df = df.withColumn("EloSwing", abs(col("WhiteRatingDiff") + col("BlackRatingDiff")))
+    else:
+        df = df.withColumn("EloSwing", lit(None).cast("int"))
+
+    # Filter based on "EloDiff" and "EloSwing"
+    df = df.filter(
+        (col("EloDiff") <= 200) & 
+        (col("EloSwing") <= 50)
+    )
+
+    # Add Opening Comment to Moves
+    df = df.withColumn("Moves", concat(lit("{ "), col("ECO"), lit(" "),  col("Opening"), lit(" } "), col("Moves")))
+
+    # Create Result Comment & add to Moves
+    df = df.withColumn(
+        "ResultComment",
+        when(col("Result") == "1/2-1/2", "Draw")
+        .when((col("Result") == "1-0") & (col("Termination") == "Time forfeit"), "White wins on time")
+        .when((col("Result") == "0-1") & (col("Termination") == "Time forfeit"), "Black wins on time")
+        .when((col("Result") == "1-0") & (col("Termination") == "Normal") & regexp_replace(col("Moves"), r"\[\%eval #\d+\]", "").contains("#"), "White wins by checkmate")
+        .when((col("Result") == "0-1") & (col("Termination") == "Normal") & regexp_replace(col("Moves"), r"\[\%eval #\d+\]", "").contains("#"), "Black wins by checkmate")
+        .when((col("Result") == "1-0") & (col("Termination") == "Normal") & ~regexp_replace(col("Moves"), r"\[\%eval #\d+\]", "").contains("#"), "White wins by resignation")
+        .when((col("Result") == "0-1") & (col("Termination") == "Normal") & ~regexp_replace(col("Moves"), r"\[\%eval #\d+\]", "").contains("#"), "Black wins by resignation")
+        .otherwise("End of Game")
+    )
+    df = df.withColumn("Moves", concat(col("Moves"), lit(" { "), col("ResultComment"), lit(" }")))
+
+    # Trim Event
+    df = df.withColumn("Event", trim(regexp_replace(df["Event"], "(http\S+)|(game|tournament|Rated|swiss)", "")))
+    # Replace "Standard" with "Rapid" in "Event" column
+    df = df.withColumn("Event", when(col("Event") == "Standard", "Rapid").otherwise(col("Event")))
+
+    return df
+
+def write_file(df):
+    # Write games to silver dir
+    df.write.partitionBy("UTCDate").mode("append").parquet(silver_dir)
+
+def process_file(file_name):
+    # Process games in the following order
+    df = parse_games(file_name)
+    df = filter_games(df)
+    write_file(df)
+
+# COMMAND ----------
+
+from concurrent.futures import ThreadPoolExecutor
+import os
+
+# List all files in bronze directory
+file_list = dbutils.fs.ls(bronze_dir)
+file_names = [file.name for file in file_list]
+
+# Function to parallelize the processing using ThreadPoolExecutor
+def parallel_process(file_names):
+    with ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
+        executor.map(process_file, file_names)
+
+parallel_process(file_names)
+
+# COMMAND ----------
+
+# Display parsed games
+result = spark.read.parquet(silver_dir)
+display(result.count())
+display(result)
---
+++ databricks/3-sample-games.py
@@ -0,0 +1,137 @@
+# Databricks notebook source
+file_month = dbutils.widgets.get("month")
+clear_gold = dbutils.widgets.get("clear_gold")
+
+silver_dir = "/Volumes/main/default/vol-silver/" + f"{file_month}/"
+gold_all_keys_dir =  "/Volumes/main/default/vol-gold/Keys"
+
+gold_games_dir = "/Volumes/main/default/vol-gold/Games/" + f"{file_month}/"
+gold_keys_dir = "/Volumes/main/default/vol-gold/Keys/" + f"{file_month}/"
+gold_keys_consolidated_dir = "/Volumes/main/default/vol-gold/Keys/Consolidated"
+
+if clear_gold == 'true':
+    dbutils.fs.rm(gold_games_dir, True)
+    dbutils.fs.rm(gold_keys_dir, True)
+    dbutils.fs.mkdirs(gold_games_dir)
+    dbutils.fs.mkdirs(gold_keys_dir)
+
+print(f"Sampling games from: {silver_dir} \n to {gold_games_dir}")
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col, lit, min, concat, row_number, max
+from pyspark.sql import Window
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from pyspark.sql import DataFrame
+
+df = spark.read.parquet(silver_dir)
+
+grouped_df = df.groupBy("Event", "EloRange").count()
+
+# Get the distinct event groups as a list
+event_groups = grouped_df.select("Event").distinct().collect()
+event_groups = [row["Event"] for row in event_groups]
+
+# Initialize an empty DataFrame with the same schema as df
+all_df = spark.createDataFrame([], schema=df.schema)
+
+def process_event(event: str) -> DataFrame:
+    # Process each Event separately
+    df_group = grouped_df.where(col("Event") == event)
+
+    # Get the smallest count of games across all EloRanges 
+    min_count = df_group.agg(min(col('count'))).collect()[0][0]
+
+    # Calculate the sample fraction - ensure that counts across all EloRanges are equal to the min_count
+    df_group = df_group.withColumn('sample_fraction', lit(min_count) / col('count'))
+
+    # Extract the columns "EloRange" and "sample_fraction" and collect them as a list of rows
+    rows = df_group.select('EloRange', 'sample_fraction').collect()
+
+    # Construct the sample dictionary
+    sample_dict = {row['EloRange']: row['sample_fraction'] for row in rows}
+
+    # Sample the source df
+    sampled_df = df.where(col("Event") == event)
+    sampled_df = sampled_df.sampleBy("EloRange", fractions=sample_dict, seed=10)
+    
+    return sampled_df
+
+# Use ThreadPoolExecutor to process each event group in parallel
+with ThreadPoolExecutor() as executor:
+    futures = {executor.submit(process_event, event): event for event in event_groups}
+
+    for future in as_completed(futures):
+        event = futures[future]
+        try:
+            result_df = future.result()
+            all_df = all_df.union(result_df)
+        except Exception as exc:
+            print(exc)
+
+
+# Add a 'NumericGameID' column using the row_number function
+window_spec = Window.orderBy('Event')
+all_df = all_df.withColumn('NumericGameID', row_number().over(window_spec))
+
+# Replace GameID with NumericGameID, rename to ID
+all_df = all_df.withColumn('GameID', concat(lit(file_month), lit('-'), col('NumericGameID')))
+all_df = all_df.withColumn('ID', col('GameID'))
+
+# Write games to gold dir
+all_df.write.partitionBy('Event','EloRange').mode("overwrite").parquet(gold_games_dir)
+
+# Display result
+all_df_result = all_df.groupBy("Event", "EloRange").count()
+display(all_df_result)
+
+# Create a key df based on sampled games
+key_df = all_df.groupBy("Event").agg(min("NumericGameID").alias("MinGameID"), max("NumericGameID").alias("MaxGameID"))
+key_df = key_df.withColumn("Month", lit(file_month))
+key_df = key_df.withColumn("KeyID", concat(lit(file_month), lit("-"), lit(col("Event"))))
+
+# Write keys to gold dir
+key_df.write.mode("overwrite").parquet(gold_keys_dir)
+
+display(key_df)
+
+# COMMAND ----------
+
+from functools import reduce
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import struct, collect_list, create_map,col
+
+# Function to read all folders in keys_path and combine them into one DataFrame
+def read_and_combine_dataframes(keys_path):
+    # List all folders in keys_path
+    folders = [file.path for file in dbutils.fs.ls(keys_path) if 'Consolidated' not in file.path]
+    print(folders)
+    
+    # Read each folder as a DataFrame and store them in a list
+    dfs = [spark.read.parquet(folder) for folder in folders]
+    
+    # Combine all DataFrames into one DataFrame using union
+    combined_df = reduce(DataFrame.union, dfs)
+    
+    return combined_df
+
+# Read and combine all game DataFrames
+df = read_and_combine_dataframes(gold_all_keys_dir)
+
+df = df.withColumnRenamed("MinGameID", "Min")
+df = df.withColumnRenamed("MaxGameID", "Max")
+
+# Create a struct column for each month
+df = df.withColumn("Range", struct("Min", "Max")) \
+       .drop("Min", "Max")
+
+# Group by Event and collect Range structs into a list
+df = df.groupBy("Event").agg(collect_list(struct("Month", "Range")).alias("GameIDs"))
+
+# Create ID column
+df = df.withColumn("ID", col("Event"))
+
+# Write consolidated keys to gold dir
+df.write.mode("Overwrite").json(gold_keys_consolidated_dir)
+
+display(df)
