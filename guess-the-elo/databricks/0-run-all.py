f32301d3e5ea5b1c1641ad3931510ce8e44da02e
Hieu Ngo <89481020+hieuimba@users.noreply.github.com>
2024-07-30 20:28:07 -0500

    Update 0-run-all.py

--- guess-the-elo/databricks/0-run-all.py
+++ guess-the-elo/databricks/0-run-all.py
@@ -2,7 +2,7 @@
 file_month = dbutils.widgets.get("month")
 
 # COMMAND ----------
-
+# test
 dbutils.notebook.run("1-decompress-file", 0, {"clear_bronze": "false", "month":file_month})
 dbutils.notebook.run("2-parse-games", 0, {"clear_silver": "false", "month":file_month})
 dbutils.notebook.run("3-sample-games", 0, {"clear_gold": "false", "month":file_month})
